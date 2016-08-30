d49bbe04d15fb4f35fc60256cd31823b044aaeda
xiaobai050 <xiaobai050@gmail.com>
2016-08-30 19:39:19 +0800

    c+s+f

--- src/pid3/main.java
+++ src/pid3/main.java
@@ -8,19 +8,16 @@ public class main {
 		test("bbbbb");
 		test("pwwkew");
 	}
-	
-	
+
 	private static void test(String s) {
-		int len=s.length();
-		for(int i=0;i<len;i++){
+		int len = s.length();
+		for (int i = 0; i < len; i++) {
 			System.out.print(i);
 		}
 		System.out.println();
 		System.out.println(s);
-		
-		
-		
-		Solution solution=new Solution();
+
+		Solution solution = new Solution();
 		solution.lengthOfLongestSubstring(s);
 	}
 }
