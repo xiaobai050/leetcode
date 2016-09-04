67c6b298afd5a1874cf41dd5b5b4ef99d241432e
xiaobai050 <xiaobai050@gmail.com>
2016-09-04 22:26:23 +0800

    pid8 未完成

---
+++ src/pid8/Solution.java
@@ -0,0 +1,57 @@
+package pid8;
+
+public class Solution {
+	public int myAtoi(String str) {
+		int rtn = 0;
+		int begin = 0;
+		int end = 0;
+		int len = str.length();
+		boolean negFlag=false;
+
+		if (len == 0)
+			return 0;
+		char[] strArray = str.toCharArray();
+		for (char ito : strArray) {
+			if (ito > '0' && ito < '9') { // 出现数字
+				break;
+			}
+
+			else if (ito == ' ') { // 空格，继续
+				begin++;
+				continue;
+			} else
+				return 0;
+		}
+		if(strArray[begin]=='+')begin++;
+		else if(strArray[begin]=='-'){negFlag=true;begin++;}
+
+		for (end = begin; end < len; end++) {
+			if (strArray[end] > '0' && strArray[end] < '9') { // 出现数字
+				continue;
+			} else
+				break;
+		}
+
+		String temp = String.valueOf(strArray, begin, end - begin);
+		// System.out.println("temp= "+temp);
+		if (temp.length() > 10)
+			if(negFlag)rtn = Integer.MIN_VALUE;
+			else rtn=Integer.MAX_VALUE;
+		
+		else if (temp.length() < 10)
+			if(negFlag)rtn = -Integer.valueOf(temp);
+			else rtn = Integer.valueOf(temp);
+		
+		else 
+			if(negFlag){
+				if (temp.compareTo("2147483648") > 0)
+					rtn = Integer.MIN_VALUE;
+			}
+			else{
+				if (temp.compareTo("2147483647") > 0)
+					rtn=Integer.MAX_VALUE;
+			}
+
+		return rtn;
+	}
+}
---
+++ src/pid8/main.java
@@ -0,0 +1,53 @@
+package pid8;
+
+/**
+ * 8. String to Integer (atoi) Implement atoi to convert a string to an integer.
+ * Hint: Carefully consider all possible input cases. If you want a challenge,
+ * please do not see below and ask yourself what are the possible input cases.
+ * Notes: It is intended for this problem to be specified vaguely (ie, no given
+ * input specs). You are responsible to gather all the input requirements up
+ * front.
+ * 
+ * @author °×
+ *
+ */
+public class main {
+	public static void main(String[] args) {
+		String[] testTable = {
+//				"+ 1",
+//				" 123+-",			
+//				" /",
+//				"2 ",
+//				" 3 ",
+//				" 4/5",
+//				"6f7",
+//				"8e9",
+//				"/*8E-9",	//0
+//				"/2",
+//				".3",
+				"2147483648",
+				"11876562876n",
+				"2147483648",
+				"-1",
+				"+ 1",
+				"-2147483648",
+				"-2147483649",
+		};
+		for (String ito : testTable)
+			test(ito);
+	}
+
+	private static void test(String ito) {
+		System.out.println("----------------");
+		System.out.print("before='"+ito+"'	");
+		
+		Solution solution=new Solution();
+		int rtn=solution.myAtoi(ito);
+
+		System.out.println("after=	"+rtn);
+		System.out.println();
+
+		
+	}
+
+}
