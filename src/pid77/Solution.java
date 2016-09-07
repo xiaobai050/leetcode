631c5d6605593384da9197e7f3494e1fd5187f2e
xiaobai050 <xiaobai050@gmail.com>
2016-09-07 21:56:40 +0800

    pid77 complete

--- src/pid77/Solution.java
+++ src/pid77/Solution.java
@@ -5,27 +5,28 @@ import java.util.List;
 
 public class Solution {
 
+	List<List<Integer>> rtn = new ArrayList<List<Integer>>();
 	List<Integer> temp = new ArrayList<Integer>();
-	
-    List<List<Integer>> rtn = new ArrayList<List<Integer>>();;
-	
-    public List<List<Integer>> combine(int n, int k) {
-    	temp.add(0);
-    	temp.add(0);
-    	solve(0,k,n,1);
-		return rtn;        
-    }
-    
-    public void solve (int index,int k,int n,int start){
-    	if(index==k){
-    		rtn.add(temp);
-    		//System.out.println(rtn);
-    		return;
-    	}
-    	for(int i=start;i<=n;i++){
-    		temp.set(index,i);
-    		//System.out.println("i="+i+" index="+index);
-    		solve(index+1,k,n,i+1);    		
-    	}
-    }
+
+	public List<List<Integer>> combine(int n, int k) {
+		for (int i = 0; i < k; i++)
+			temp.add(0);
+		solve(0, k, n, 1);
+		return rtn;
+	}
+
+	public void solve(int index, int k, int n, int start) {
+		if (index == k) {
+			List<Integer> tempAdd = new ArrayList<Integer>(temp); // 需要新建一個List，否則set會覆蓋之前已經add的List
+
+			rtn.add(tempAdd);
+			// System.out.println(rtn);
+			return;
+		}
+		for (int i = start; i <= n; i++) {
+			temp.set(index, i);
+			// System.out.println("i="+i+" index="+index);
+			solve(index + 1, k, n, i + 1);
+		}
+	}
 }
--- src/pid77/main.java
+++ src/pid77/main.java
@@ -2,17 +2,31 @@ package pid77;
 
 import java.util.List;
 
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out
+ * of 1 ... n.
+ * 
+ * For example, If n = 4 and k = 2, a solution is:
+ * 
+ * [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4], ]
+ * 
+ * @author Administrator
+ *
+ */
 public class main {
 	public static void main(String[] args) {
 		int n = 4;
 		int k = 2;
 
-		test(n,k);
+		test(n, k);
+		test(6, 3);
+		test(1, 1);
 	}
+
 	private static void test(int n, int k) {
 		Solution solution = new Solution();
-		List<List<Integer>>rtn;
-		rtn=solution.combine(n, k);
-//		System.out.println(rtn);
+		List<List<Integer>> rtn;
+		rtn = solution.combine(n, k);
+		System.out.println(rtn);
 	}
 }
