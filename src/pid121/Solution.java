3869b7e8abd0402e36259c4fb76e1accefb11830
xiaobai050 <xiaobai050@gmail.com>
2016-09-19 22:56:00 +0800

    121 and 171

---
+++ src/pid121/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0)return 0;
+        int maxSub=0;
+        int minNum=prices[0];
+        for(int ito:prices){
+            minNum=min(minNum,ito);
+            maxSub=max(ito-minNum,maxSub);
+        }
+        return maxSub;
+    }
+    
+    public int min(int i,int j) {
+        return i<j?i:j;
+    }
+    
+    public int max(int i,int j) {
+        return i>j?i:j;
+    }
+}
---
+++ src/pid171/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        int len=s.length();
+        if(len==0)return 0;
+        int rtn=0;
+        for (int i=0;i<len;i++){
+            char ch=s.charAt(i);
+            if(ch>='a'&&ch<='z')
+                ch+=('A'-'a');
+            rtn*=26;
+            rtn+=ch-'A'+1;
+            
+        }
+        return rtn;
+    }
+}
