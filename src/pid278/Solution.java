374bbaa5628e78ca670f421f7e6250dab1d6b39c
xiaobai050 <xiaobai050@gmail.com>
2016-09-19 22:21:19 +0800

    pid 278 网页过

---
+++ src/pid278/Solution.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int begin=1;
+        int end=n;
+        
+        while(begin<end-1){
+            int middle=begin/2+end/2+(begin%2+end%2)/2;
+            if(isBadVersion(middle))end=middle;
+            else begin=middle;
+        }
+        
+        if(isBadVersion(begin))return begin;
+        return end;
+    }
+}
