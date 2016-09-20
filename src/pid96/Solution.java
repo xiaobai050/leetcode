267d9a7ec2b3cb5a409c9d91a51964e5468e9f27
xiaobai050 <xiaobai050@gmail.com>
2016-09-20 16:28:15 +0800

    nothing

--- src/pid96/Solution.java
+++ src/pid96/Solution.java
@@ -1,21 +1,23 @@
 package pid96;
 
 public class Solution {
-    public int numTrees(int n) {
-        
-        if(n<=1)return 1;
-        if(n==2)return 2;
-        
-        int [] table= new int [n+1];
-        
-        table[0]=1;
-        table[1]=1;
-        table[2]=2;
-        
-        for(int i=2;i<n;i++)
-            for(int j=0;j<=i;j++)
-                table[i+1]+=table[i-j]*table[j];
-        
-        return table[n];
-    }
+	public int numTrees(int n) {
+
+		if (n <= 1)
+			return 1;
+		if (n == 2)
+			return 2;
+
+		int[] table = new int[n + 1];
+
+		table[0] = 1;
+		table[1] = 1;
+		table[2] = 2;
+
+		for (int i = 2; i < n; i++)
+			for (int j = 0; j <= i; j++)
+				table[i + 1] += table[i - j] * table[j];
+
+		return table[n];
+	}
 }
--- src/pid96/main.java
+++ src/pid96/main.java
@@ -1,7 +1,9 @@
 package pid96;
 
 /**
- * Unique Binary Search Trees Given n, how many structurally unique BST's
+ * Unique Binary Search Trees 
+ * 
+ * Given n, how many structurally unique BST's
  * (binary search trees) that store values 1...n?
  * 
  * For example, Given n = 3, there are a total of 5 unique BST's.
@@ -29,9 +31,9 @@ public class main {
 		long begin = System.currentTimeMillis();
 		rtn = solution.numTrees(ito);
 		long end = System.currentTimeMillis();
-		System.out.println("rtn=" +rtn);
+		System.out.println("rtn=" + rtn);
 		System.out.println();
-		System.out.println("ºÄÊ±£º"+(end-begin)+"ms");
+		System.out.println("ºÄÊ±£º" + (end - begin) + "ms");
 		System.out.println("--------");
 	}
 }
