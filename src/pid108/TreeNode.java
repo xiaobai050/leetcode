59893762195733bf8360ac6025aff66c101f973a
xiaobai050 <xiaobai050@gmail.com>
2017-03-19 13:49:49 +0800

    ssh

--- src/pid108/TreeNode.java
+++ src/pid108/TreeNode.java
@@ -1,11 +1,11 @@
 package pid108;
 
 public class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
 
-	TreeNode(int x) {
+	public TreeNode(int x) {
 		val = x;
 	}
 
